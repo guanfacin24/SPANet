e417fc3ea272d9034052f4860933e6dd58f5a7aa
guanfacin24 <kai.fabian.adamowicz@cern.ch>
2025-07-16 11:36:33 +0200

    Hotfixed an issue in which checkpoints might be directories

--- spanet/train.py
+++ spanet/train.py
@@ -174,7 +174,7 @@ def main(
         ModelCheckpoint(
             #verbose=options.verbose_output,
             verbose=False,
-            filename='{epoch}-{step}-{' + options.central_metric[0] +':.3f}',
+            filename='{epoch}-{step}-{:.3f}',
             monitor=options.central_metric[0],
             save_top_k=3,
             mode=options.central_metric[1],
